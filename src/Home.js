6fccfa1a8d9cb426d0e1cb7aa33fb09567bbef6e
AFDHAL <Nejib2021@microacess.com>
2022-09-11 15:19:53 +0100

    new update

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+const Home = () => {
+    return ( 
+    <div>
+    <Link to="/About">About</Link>
+    <h2>Home Page</h2> 
+    </div>
+    );
+   }
+   export default Home;
